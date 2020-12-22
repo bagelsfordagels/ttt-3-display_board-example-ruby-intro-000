31b5f83484358e4868d5c9f2aed8ce5593078fdb
benurgh <benurgh@gmail.com>
2020-12-22 19:00:00 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,6 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board(move = nil)
-  puts " #{move} |  |    "
+  puts " #{move}  |  |    "
   puts "----------"
   puts "   |  |    "
   puts "----------"
